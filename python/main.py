bf591772d834d00662475e317cda3a2244754b2e
lsalazar1 <lmsalazar78011@gmail.com>
2019-11-04 00:20:15 -0600

    Python program created for posting fake data into MongoDB

---
+++ python/main.py
@@ -0,0 +1,29 @@
+from config import getConnection
+import pymongo
+import dns
+
+if __name__ == '__main__':
+    connection = pymongo.MongoClient(getConnection())
+
+    # Specify name of database
+    database = connection['test2']
+
+    # Specify name of collection
+    collection = database['testCollection']
+
+    # Fake data we're sending to MongoDB -> Also JSON
+    testData = {
+        "parkingLot": "S.R. Collins",
+        "Sensors": [
+            {
+                "sensorID": "001",
+                "isVacant": False
+            },
+            {
+                "sensorID": "002",
+                "isVacant": True
+            }
+        ]
+    }
+
+    collection.insert_one(testData)
