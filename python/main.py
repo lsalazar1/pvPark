8a1ca7923418963216f534450bc8661d75389bec
lsalazar1 <lmsalazar78011@gmail.com>
2019-11-18 22:18:04 -0600

    testing fakeData post

--- python/main.py
+++ python/main.py
@@ -12,17 +12,8 @@ if __name__ == '__main__':
     database = connection['test']
 
     # Find a collection within the test db called 'parking'
-    collection = database['parking']
+    collection = database['SRCollins']
 
-    # Create an object
-    SR_Collins = ParkingLot('SRCOllins', 168)
+    fakeData = { "_id": "SRC0", "isVacant": True}
 
-    # Create a sensor
-    SR_Collins.createUltrasonic(echo = 24, trigger = 23)
-
-    print(SR_Collins.createDictionary())
-    
-    # Post dictionary to db
-    collection.insert_one(SR_Collins.createDictionary())
-
-    print('Hello')
+    collection.insert_one(fakeData)
