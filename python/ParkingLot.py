aaf053c4d3b1a5000c5eea724e624a6770f1d043
lsalazar1 <lmsalazar78011@gmail.com>
2019-11-18 21:56:26 -0600

    Post a document each time us is created

--- python/ParkingLot.py
+++ python/ParkingLot.py
@@ -6,14 +6,22 @@ import pymongo
 import dns
 
 class ParkingLot:
-    # Basically a constructor
+    # When initializing an instance of ParkingLot, connect to MongoDB
     def __init__(self, lotName, totalSpots):
         self.lotName = lotName
         self.totalSpots = totalSpots
         self.parkingLot = {}
         self.sensorsList = []
 
-        #
+        # Connect to mongodb using connection string
+        self.connection = pymongo.MongoClient(getConnection())
+
+        # Create/Search for a database called test
+        self.database = self.connection['test']
+
+        # Create/Search for a collection with lot name
+        self.collection = self.database[self.lotName]
+
 
     def countSensors(self):
         return len(self.sensorsList)
@@ -32,7 +40,7 @@ class ParkingLot:
     def createUltrasonic(self, echo, trigger):
         # Each index in sensorsList will have this object per sensor
         info = {
-            "sensorID": "",
+            "_id": "",
             "isVacant": False,
             "echo": echo,
             "trigger": trigger
@@ -41,14 +49,14 @@ class ParkingLot:
         sensor = DistanceSensor(echo = echo, trigger = trigger, max_distance = 0.02, threshold_distance=0.005)
 
         # Naming convention is first three chars of lot name and place in sensorsList
-        info["sensorID"] = self.lotName[:3] + str(self.countSensors())
+        info["_id"] = self.lotName[:3] + str(self.countSensors())
 
         print("Sensor %s is initializing" % info["sensorID"])
 
         # Alters info["isVacant"] value based on sensor's reading... use sensor as a param 
         info["isVacant"] = self.isVacant(sensor)
 
-        self.sensorsList.append(info)
+        self.collection.insert_one(info)
 
     # Checks if parking spot is vacant using sensor as param
     def isVacant(self, sensor):
@@ -65,3 +73,7 @@ class ParkingLot:
         self.parkingLot["sensors"] = self.sensorsList
 
         return self.parkingLot
+
+x = ParkingLot("SR Collins", 168)
+
+x.createUltrasonic(echo=23, trigger=24)
