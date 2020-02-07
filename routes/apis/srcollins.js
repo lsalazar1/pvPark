ab46b32d3f1fa09b2a8ff443e628239df521385a
lsalazar1 <lmsalazar78011@gmail.com>
2020-02-06 18:25:19 -0600

    Removed hard-coded json for SRCollins

--- routes/apis/srcollins.js
+++ routes/apis/srcollins.js
@@ -2,26 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../../middleware/auth');
+const SrCollins = require('../../models/SrCollins');
 
 // @route       api/srcollins
-// @desc        GET api/srcollins
+// @desc        GET SR Collins JSON
 // @access      PRIVATE
 router.get('/', async (req, res) => {
     try{
-        // This variable is a sample object of what the full app would have 
-        const srcollins = {
-            "lotName" : "SRCollins",
-            "sensors" : [
-                { "_id" : "SRC0", "isVacant" : true },
-                { "_id" : "SRC0", "isVacant" : true },
-                { "_id" : "SRC0", "isVacant" : true },
-                { "_id" : "SRC0", "isVacant" : true },
-                { "_id" : "SRC0", "isVacant" : true }
-            ]
-        }
-
-        // Send JSON object as a successful response
-        res.send(srcollins);
+        let lot = await SrCollins.findOne();
+        res.send(lot);
     } catch (err) {
         res.status(500).json({'msg' : 'Error 500'});
         console.error(err.message);
