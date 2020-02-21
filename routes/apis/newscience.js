2c6c3a581d82d3e231403fd2885228214d4855a7
lsalazar1 <lmsalazar78011@gmail.com>
2020-02-21 17:55:56 -0600

    Altering test data for api/newscience

--- routes/apis/newscience.js
+++ routes/apis/newscience.js
@@ -5,20 +5,56 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const NewScience = require('../../models/NewScience');
 
+let data = {
+    'lotName': 'newscience',
+    'sensors': [
+        {
+            '_id': 'new0',
+            'isVacant': true
+        },
+        {
+            '_id': 'new1',
+            'isVacant': false
+        },
+        {
+            '_id': 'new2',
+            'isVacant': false
+        },
+        {
+            '_id': 'new3',
+            'isVacant': false
+        },
+        {
+            '_id': 'new4',
+            'isVacant': true
+        },
+        {
+            '_id': 'new5',
+            'isVacant': false
+        },
+        {
+            '_id': 'new6',
+            'isVacant': true
+        },
+        {
+            '_id': 'new7',
+            'isVacant': true
+        },
+        {
+            '_id': 'new8',
+            'isVacant': true
+        },
+        {
+            '_id': 'new9',
+            'isVacant': true
+        },
+    ]
+};
+
 // @route       api/newscience
 // @desc        GET New Science JSON
 // @access      PRIVATE
 router.get('/', async (req, res) => {
-    let data = {
-        'lotName': 'newscience',
-        'sensors': [
-            {
-                '_id': 'new0',
-                'isVacant': true
-            }
-        ]
-    };
-
     // Destructure fields in data JSON above...
     let {lotName, sensors} = data;
 
@@ -30,7 +66,6 @@ router.get('/', async (req, res) => {
         let newLot = new NewScience({lotName, sensors});
         await newLot.save()     // Saves the new lotObj to the db
     }
-
 
     // Will return a document from the newscience collection as a response... Otherwise there may be some problem reaching the server
     try {
