17dc44943a1de33ff32fa040d9e7d0531d38877c
rouoba <briceouoba@gmail.com>
2020-02-22 13:28:46 -0600

    created Business AG lot

---
+++ models/BusinessAG.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const BusinessAGSchema = new mongoose.Schema({
+    lotName: {
+        default: "Business AG",
+        type: String
+    },
+    sensors: [
+        { _id: String , isVacant: Boolean }
+    ]
+})
+
+module.exports = Businessag = mongoose.model("businessag", BusinessAGSchema)
