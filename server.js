5ed42975bd0ae9e77dd123970a8266d291631a71
rouoba <briceouoba@gmail.com>
2020-02-26 11:57:14 -0600

    added entry for MSC lot in server.js file

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ app.use('/api/users', require('./routes/apis/users'));
 app.use('/api/auth', require('./routes/apis/auth'));
 app.use('/api/srcollins', require('./routes/apis/srcollins'));
 app.use('/api/newscience', require('./routes/apis/newscience'));
-app.use('/api/businessag', require('./routes/apis/businessag'))
+app.use('/api/businessag', require('./routes/apis/businessag'));
+app.use('/api/msc', require('./routes/apis/msc'));
 
 // Activate express on port decalred... 
 app.listen(PORT, () => console.log(`Server active on Port ${PORT}`));
