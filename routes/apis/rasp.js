fa219c4c78bb900b4f277ee2d6ff4ab8ede3c6d9
lsalazar1 <lmsalazar78011@gmail.com>
2020-02-27 12:27:23 -0600

    Testing rasp

--- routes/apis/rasp.js
+++ routes/apis/rasp.js
@@ -57,25 +57,12 @@ let data = {
 // @desc        GET rasp JSON
 // @access      PRIVATE
 router.get('/', async (req, res) => {
-    // Destructure fields in data JSON above...
-    let {lotName, sensors} = data;
-
-    // Returns a document in the newscience collection...
-    let lot = await Rasp.findOne();
-    
-    // If there are no documents in the collection, this if-statement will create a lot object with the destructured fields in line 22
-    if (!lot) {
-        let newLot = new Rasp({lotName, sensors});
-        await newLot.save()     // Saves the new lotObj to the db
-    }
-
-    // Will return a document from the newscience collection as a response... Otherwise there may be some problem reaching the server
     try {
-        lot = await Rasp.findOne();
+        let lot = await SrCollins.findOne();
         res.send(lot);
-    } catch (error) {
-        // Returns a 500 Status Code and a json
-        res.status(500).json({'msg': 'Error 500'})
+    } catch (err) {
+        res.status(500).json({'msg' : 'Error 500'});
+        console.error(err.message);
     }
 });
 
