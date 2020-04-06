10961163bd11eb70363e6922d791072f60ad44ad
lsalazar1 <lmsalazar78011@gmail.com>
2020-04-06 06:06:23 -0500

    Adding HTTP POST test for api/users

--- test.js
+++ test.js
@@ -5,6 +5,29 @@ const baseURI = "https://blooming-mountain-10766.herokuapp.com/";
 
 const successCode = 200;
 
+const config = {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}
+
+const registerData = [
+    {
+        "username": "orange_guy10",
+        "password": "ackabacka95",
+        "email": "dtrump@pvamu.edu",
+        "name": "Donald Trump",
+        "expected": 200
+    },
+    {
+        "username": "uncle_joe2005",
+        "password": "ackabacka95",
+        "email": "jbiden@pvamu.edu",
+        "name": "Joe Biden",
+        "expected": 200
+    }
+]
+
 // Testing if backend is connected
 axios.get(baseURI)
     .then(res => {
@@ -16,7 +39,7 @@ axios.get(baseURI)
         exit(1);
     });
 
-// Testing GET api/srcollins endpoint
+//  GET api/srcollins
 axios.get(baseURI+"api/srcollins")
     .then(res => {
         let lotName = res.data.lotName;
@@ -32,7 +55,7 @@ axios.get(baseURI+"api/srcollins")
         exit(1);
     });
 
-// Testing GET api/newscience
+// GET api/newscience
 axios.get(baseURI+"api/newscience")
     .then(res => {
         let lotName = res.data.lotName;
@@ -48,7 +71,7 @@ axios.get(baseURI+"api/newscience")
         exit(1);
     });
 
-// Testing api/businessag
+// GET api/businessag
 axios.get(baseURI+"api/businessag")
     .then(res => {
         let lotName = res.data.lotName;
@@ -64,8 +87,9 @@ axios.get(baseURI+"api/businessag")
         exit(1);
     });
 
+// GET api/msc
 axios.get(baseURI+"api/msc")
-    .then( res => {
+    .then(res => {
         let lotName = res.data.lotName;
         let responseStatus = res.status;
 
@@ -78,3 +102,24 @@ axios.get(baseURI+"api/msc")
         console.error(err.message);
         exit(1);
     });
+
+// POST api/users
+for (let i = 0; i < registerData.length; i++) {
+
+    let {username, name, password, email, expected} = registerData[i];
+    let num_test_passing = 0;
+
+    let body = JSON.stringify({username, name, password, email});
+
+    axios.post(baseURI + 'api/users', body, config)
+        .then(res => {
+            let responseStatus = res.status;
+
+            assert.deepEqual(responseStatus, expected, `Expected ${expected} but received ${responseStatus}`);
+            num_test_passing += 1;
+        })
+        .catch(err => {
+            console.error(`api/users... ${num_test_passing} case(s) passed... Failing at [${i}]`);
+            exit(1);
+        })
+}
